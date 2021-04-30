0343c9c445674223b9158dbfb8b12354ee8d0953
Lyle Timotheus <lyle6494@gmail.com>
2021-04-30 21:43:48 +0200

    Fixed the search errors

--- public/php/search.php
+++ public/php/search.php
@@ -4,16 +4,14 @@ require_once("./functions/userInput.php");
 $searchTerm = test_input($_POST["searchTerm"]);
 $output = "";
 
-$stmt = $conn -> prepare("SELECT * FROM users WHERE fname LIKE '%$searchTerm%'");
+$stmt = $conn -> prepare("SELECT * FROM users WHERE fname LIKE '%$searchTerm%' OR lname LIKE '%$searchTerm%'");
 $stmt -> execute();
-$result = $stmt -> fetchAll();
-
-
-if(count($result) > 0){
-    $output .=  "Name:" .$result['fname'];
-}else {
-    $output .= "No users found!";
-}
+$result = $stmt -> fetch(PDO::FETCH_ASSOC);
+    if($stmt -> rowCount() > 0){
+        // $output .=  "Name:" .$result['fname'];
+    }else {
+        $output .= "No users found!";
+    }
 
 echo $output;
 ?>
