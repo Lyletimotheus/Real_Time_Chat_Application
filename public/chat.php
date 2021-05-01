d2733497e38aea0e7ea4482520bf467009eaf91b
Lyle Timotheus <lyle6494@gmail.com>
2021-05-01 23:46:51 +0200

    created chat.php

--- public/chat.php
+++ public/chat.php
@@ -4,25 +4,28 @@ $unique_id = $_SESSION['unique_id'];
 if(!isset($unique_id)){
     header("location:login.php");
 }
-require_once("php/partials/header.php")
+require_once("php/partials/header.php");
+require_once("../database/config.php");
+require_once("php/functions/userInput.php");
 ?>
 <body>
     <div class="wrapper">
         <section class="chat-area">
             <header>
             <?php 
+            $user_id = test_input($_GET['user_id']);
             $stmt = $conn -> prepare("SELECT * FROM users WHERE unique_id = :unique_id");
-            $stmt -> bindParam(':unique_id', $unique_id);
+            $stmt -> bindParam(':unique_id', $user_id);
             $stmt -> execute();
             if($stmt -> rowCount() > 0){
-                $data = $stmt -> fetch();
+                $data = $stmt -> fetch(PDO::FETCH_ASSOC);
             }
             ?>
                 <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-                <img src="images/img.jpg" alt="">
+                <img src="php/images/<?php echo $data['img'] ?>" alt="">
                 <div class="details">
-                    <span>Lyle Timotheus</span>
-                    <p>Active Now</p>
+                    <span><?php echo $data['fname']. " " . $data['lname'] ?></span>
+                    <p><?php echo $data['status'] ?></p>
                 </div>          
             </header>
             <div class="chat-box">
