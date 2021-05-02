9610c5aa000439e3343858ce2ce1d08d0cc4601c
Lyle Timotheus <lyle6494@gmail.com>
2021-05-02 22:05:10 +0200

    Changes to the chat file

--- public/php/data.php
+++ public/php/data.php
@@ -1,5 +1,13 @@
 <?php
 while($data = $stmt -> fetch()){
+    $stmt = $conn -> prepare("SELECT * FROM messages WHERE (incoming_msg_id = :incoming_unique_id
+                            OR outgoing_msg_id = :outgoing_unique_id) AND 
+                            (outgoing_msg_id = :incoming_unique_id 
+                            OR incoming_msg_id = :outgoing_unique_id)");
+                            
+    $stmt -> bindParam(':incoming_unique_id', $data['unique_id']);
+    $stmt -> bindParam(':outgoing_unique_id', $data['unique_id']);
+    $stmt -> execute();
 
     $output .= '<a href="chat.php?user_id='.$data['unique_id'].'">
     <div class="content">
